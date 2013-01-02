b044c0df56537ac499ff8f19e8f94c152c940f94
Xavier DUBREUIL <xavier.dubreuil@xaifiet.com>
2013-01-03 00:22:03 +0100

    Add strip tags to line function

--- cfg/functions.php
+++ cfg/functions.php
@@ -2,7 +2,7 @@
 
 function line($start, $field)
 {
-    $array = preg_split('/$\R?^/m', $field);
+    $array = preg_split('/$\R?^/m', strip_tags($field));
     foreach ($array as $item) {
         if (!strncmp($start, $item, strlen($start))) {
             return trim(substr($item, strpos($item, ':')+1));
