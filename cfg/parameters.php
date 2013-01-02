110630f54c55322fe819861ef5feb634379fa244
Xavier DUBREUIL <xavier.dubreuil@xaifiet.com>
2013-01-02 12:18:36 +0100

    Update cfg/parameters.php

--- cfg/parameters.php
+++ cfg/parameters.php
@@ -1,6 +1,6 @@
 <?php
 
-    $param['site']['name']            = 'Japan-Shin';
+    $param['site']['name']            = 'Manga';
     $param['site']['uri']['home']     = 'http://192.168.42.48/manga/';
     $param['site']['uri']['pages']    = 'http://192.168.42.48/manga/content/comics/';
     $param['site']['uri']['uri']      = 'http://192.168.42.48/manga/iManga/index.php';
